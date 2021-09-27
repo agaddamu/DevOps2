352ef3675a10508e72016d69e24274fad119b2d8
Ankur Singh <ankursingh@expedia.com>
2021-09-27 16:45:32 +0530

    Corrected scripted pipeline

--- JenkinsPipelineExamples/ScriptedPipeline.groovy
+++ JenkinsPipelineExamples/ScriptedPipeline.groovy
@@ -1,12 +1,14 @@
-node {
-    stage('Build') {
+//Scripted pipeline
+node('your-node') {
+    stage('Build') { //Build stage
+        // Referring jenkins environment variable BRANCH_NAME
         if (env.BRANCH_NAME == 'master') {
             echo 'Execute on the master branch'
         } else {
             echo 'I execute elsewhere'
         }
     }
-    stage('Test') {
+    stage('Test') { // Test stage
             try {
                 sh 'exit 1'
             }
@@ -15,8 +17,8 @@ node {
                 throw
             }
         }
-    stage("Deploy") {
-        node("linux") {
+    stage("Deploy") { // Deploy stage
+        node("linux") {  // Running this stage on node with label "linux"
             echo "One"
         }
     }
