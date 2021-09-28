f745e46cf16897b3295395d7ba1b742e442031e8
Ankur Singh <ankursingh@expedia.com>
2021-09-28 09:10:50 +0530

    Sample terraform

--- Labs/usecases/usecase-1/terraform/sample/terraform_sample_1.tf
+++ Labs/usecases/usecase-1/terraform/sample/terraform_sample_1.tf
@@ -1,23 +1,32 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
+  region = "us-west-2"
+}
+
+variable "image_id" {
+  type = string
+  default = "ami-08d70e59c07c61a3a"
+}
+
+variable "security_groups_name" {
+  type = list(string)
+  default = [
+    "s1",
+    "s2"]
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
+  ami = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+resource "aws_instance" "app_server" {
+  ami = var.image_id
+  instance_type = "t2.micro"
+  security_groups = var.security_groups_name
   tags = {
     Name = "ExampleAppServerInstance"
   }
