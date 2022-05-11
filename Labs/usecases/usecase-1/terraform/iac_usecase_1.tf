2ebffd21c2752e1c127ded5537ad36e4e09dda44
Anand Gaddamu <anandkishorereddy@gmail.com>
2022-05-11 15:24:51 -0400

    Pinned down AWS provider version

--- Labs/usecases/usecase-1/terraform/iac_usecase_1.tf
+++ Labs/usecases/usecase-1/terraform/iac_usecase_1.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.5.0"
+      region = "us-east-1"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
 variable "VPC" {
   type = string
   default = "vpc-ef705e95"
